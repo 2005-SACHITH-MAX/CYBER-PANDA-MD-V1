35ba14350906dec360952f9796d6184dde1036d5
CYBER-x-SACHIYA-SL <139337092+2005-SACHITH-MAX@users.noreply.github.com>
2023-08-05 22:07:49 +0530

    Update _help.js

--- commands/_help.js
+++ commands/_help.js
@@ -58,7 +58,7 @@ Secktor.cmd({
 \n` + '```'
                 for (const category in cmds) 
                 {
-                   str += `╭────❊ *${tiny(category)}* ❏\n` ;
+                   str += `╭────❊ *${tiny(category)}* ❊\n` ;
                    if(text.toLowerCase() == category.toLowerCase()){ str = `╭─────❊ *${tiny(category)}* ❊\n` ;      
                         for (const plugins of cmds[category]) { str += `│ ${fancytext(plugins,1)}\n` ; }
                         str += `╰━━━━━━━━━━━━━──❊\n`  ;
